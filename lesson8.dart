266b82f79526efb83cda66bf00d3e0dece5d0152
chikaasan <chyngyz.asanhodjoevv@gmail.com>
2021-03-27 11:34:44 +0300

    Function.lesson8

---
+++ lesson8.dart
@@ -0,0 +1,175 @@
+main() {
+  print(array1("GARRY"));
+  
+  
+  
+}
+List array1 (String city){
+  int x = 0;
+  List array = city.split("");
+  while(x <= array.length - 1){
+    switch(array[x]){
+      case "A":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case "B":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case "C":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case "D":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case "E":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case "F":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case "G":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case "H":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case "I":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case "J":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case "K":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case "L":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case "M":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case "N":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case "O":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case "P":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case "Q":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case "R":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case "S":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case "T":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case "U":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case "V":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case "W":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case "X":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case "Y":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case "Z":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case   "a":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case   "b":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case  "c":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case  "d":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case  "e":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case   "f":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case   "g":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case   "h":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case   "i":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case   "j":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case  "k":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case  "l":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case  "m":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case  "n":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case  "o":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case  "p":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case   "q":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case  "r":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case  "s":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case   "t":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case   "u":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case   "v":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case   "w":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case   "x":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case  "y":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;
+      case   "z":
+        print("${array[x]} = ${city.split("${array[x]}").length - 1}");
+        break;  
+      default:
+        print("Error");
+    }
+    x++;
+    
+}
+return array;
+}
